06f227b99cde6ea5942fd863466bd2d9a8c74f6e
cinderella <50831606+tangaoyang@users.noreply.github.com>
2020-07-13 22:42:07 +0800

    7.13晚上

--- MRCARC/main.m
+++ MRCARC/main.m
@@ -19,8 +19,13 @@ int main(int argc, const char * argv[]) {
 //        id obj2 = [[NSObject alloc] init];
 //    [obj2 autorelease];
 //        id obj3 = [[NSObject alloc] init];
-//        id ob = [[NSObject alloc] init];
+////        id ob = [[NSObject alloc] init];
+//    {
+//        id objj = obj;
 //        NSLog(@"[obj retainCount] == %lu", (unsigned long)[obj retainCount]);
+//        [objj release];
+//    }
+//    NSLog(@"[obj retainCount] == %lu", (unsigned long)[obj retainCount]);
 //        extern void_objc_autoreleasePoolPrint(void);
 //        void_objc_autoreleasePoolPrint();
 //        [obj release];
@@ -62,6 +67,46 @@ int main(int argc, const char * argv[]) {
 //    BOOL result = [string writeToURL: url atomically:NO encoding:NSUTF8StringEncoding error:&tmp];
 //    error = tmp;
     
-    
+//    显式转换id和void*
+//    ARC无效
+//    void *p = NULL;
+//    id obj = (id)p;
+//    [obj retain];
+//    [(id)p release];
+//    ARC无效
+//    id p = [[NSObject alloc] init];
+//    NSLog(@"%@", [p class]);
+//    [p release];
+//    ARC无效 CF框架和OC对象
+//    id obj = NULL;
+//    CFMutableArrayRef cfObj = NULL;
+//    {
+//        id obj = [[NSMutableArray alloc] init];
+////        cfObj = CFBridgingRetain(obj);
+//        cfObj = (__bridge CFMutableArrayRef)obj;
+//        CFShow(cfObj);
+////        NSLog(@"%ld", (long)CFGetRetainCount(obj));
+//        NSLog(@"%ld", (long)CFGetRetainCount(cfObj));
+////
+////        NSLog(@"%lu", [obj retainCount]);
+//    }
+//    NSLog(@"%ld", (long)CFGetRetainCount(cfObj));
+////    NSLog(@"%ld", [cfObj retainCount]);
+//    CFRelease(cfObj);
+//    NSLog(@"%lu", [obj retainCount]);
+//    60页 obj持有对象的时候cfObj不是要释放吗 怎么还可以访问
+//    {
+//        CFMutableArrayRef cfObj = CFArrayCreateMutable(kCFAllocatorDefault, 0, NULL);
+//        NSLog(@"%ld", CFGetRetainCount(cfObj));
+//        id obj = CFBridgingRelease(cfObj);
+//        NSLog(@"%ld", CFGetRetainCount(cfObj));
+//        NSLog(@"%@ %@", cfObj, obj);
+//    }
+//    P69
+//    {
+//        id __weak obj = [[NSObject alloc] init];
+////        id __weak obj = nil;
+//        NSLog(@"%@", obj);
+//    }
     return 0;
 }
