2ed6d7d708440e0bf2adde42868e7596624a33a3
cinderella <50831606+tangaoyang@users.noreply.github.com>
2020-07-13 19:50:32 +0800

    7.13下午

--- MRCARC/main.m
+++ MRCARC/main.m
@@ -14,21 +14,53 @@ int main(int argc, const char * argv[]) {
         // insert code here...
 //        NSLog(@"Hello, World!");
 //        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-        id obj = [[NSObject alloc] init];
-    [obj autorelease];
-        id obj2 = [[NSObject alloc] init];
-    [obj2 autorelease];
-        id obj3 = [[NSObject alloc] init];
-        id ob = [[NSObject alloc] init];
+//        id obj = [[NSObject alloc] init];
+//    [obj autorelease];
+//        id obj2 = [[NSObject alloc] init];
+//    [obj2 autorelease];
+//        id obj3 = [[NSObject alloc] init];
+//        id ob = [[NSObject alloc] init];
 //        NSLog(@"[obj retainCount] == %lu", (unsigned long)[obj retainCount]);
 //        extern void_objc_autoreleasePoolPrint(void);
 //        void_objc_autoreleasePoolPrint();
 //        [obj release];
 //        NSLog(@"[obj retainCount] == %lu", (unsigned long)[obj retainCount]);
 //        [obj dealloc];
-        [NSAutoreleasePool showPools];
-//    }
+//        [NSAutoreleasePool showPools];
     
+    // __weak修饰符
+//    id __weak obj = [[NSObject alloc] init];
+//    NSLog(@"[obj retainCount] == %lu", (unsigned long)[obj retainCount]);
+//    }
+//    id __weak obj = nil;
+//    {
+//        id obj0 = [[NSObject alloc] init];
+//        obj = obj0;
+//        NSLog(@"%@", obj);
+//    }
+//    NSLog(@"%@", obj);
+    
+//    __unsafe_unretained修饰符
+//    id __unsafe_unretained obj = nil;
+//    {
+//        id obj0 = [[NSObject alloc] init];
+//        obj = obj0;
+//        NSLog(@"%@", obj);
+//    }
+//    NSLog(@"%@", obj);
+    
+//    __autoreleasing修饰符
+//    NSError *error = [[NSError alloc] init];
+//    NSError * __autoreleasing *pError = &error;
+//    NSError * __autoreleasing *error2 = nil;
+//    *error2 = [[NSError alloc] init];
+//    NSError  __strong *error = nil;
+//    NSError  __autoreleasing *tmp = nil;
+//    NSError **pError = &tmp;
+//    NSString *string = @"123";
+//    NSURL *url;
+//    BOOL result = [string writeToURL: url atomically:NO encoding:NSUTF8StringEncoding error:&tmp];
+//    error = tmp;
     
     
     return 0;
